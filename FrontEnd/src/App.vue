c56ff7697ade11d507b082da476e967132db1f43
Gift Mkhoma <165139537+Emkay19@users.noreply.github.com>
2025-08-18 14:04:32 +0200

    Started the frontend

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,47 +1,141 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <header class="head">
+
+    <div class="menu">
+      <button class="menu-cover"></button>
+      <img src="/menu-logo.png" alt="" class="menu-button"></img>
     </div>
+
+
+    <div class="left-links">
+      <a href="" class="about">About</a>
+      <a href="" class="schedules">Schedules</a>
+    </div>
+
+    <img src="/campus-logo.png" alt="" class="main-logo">
+
+    <div class="right-links">
+      <a href="" class="dashboard">Dashboard</a>
+      <a href="" class="help">Help/Faq</a>
+    </div>
+
+    <div class="profile">
+      <button class="profile-cover"></button>
+      <img src="/profile-logo.png" alt="" class="profile-button"></img>
+    </div>
+
+
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+
+<script>
+
+</script>
+
+<style>
+html,
+body {
+  background-color: #319AC4;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.head {
+  display: flex;
+  padding: 15px 30px;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 140px;
+  background: linear-gradient(to bottom, black, transparent);
+  z-index: 1000;
+  box-sizing: border-box;
+  gap: 50px;
+  color: white;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.right-links {
+  display: flex;
+  gap: 80px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.dashboard {
+  text-decoration: none;
+  color: white;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.help {
+  text-decoration: none;
+  color: white;
+}
+
+.left-links {
+  display: flex;
+  gap: 80px;
+}
+
+.about {
+  text-decoration: none;
+  color: white;
+}
+
+.schedules {
+  text-decoration: none;
+  color: white;
+}
+
+.menu {
+  position: relative;
+  display: inline-block;
+}
+
+.profile {
+  position: relative;
+  display: inline-block;
+}
+
+.menu-cover {
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  margin-left: 50px;
+}
+
+.profile-cover {
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  margin-right: 110px;
+}
+
+.main-logo {
+  width: 400px;
+  height: 90px;
+}
+
+.menu-button {
+  position: absolute;
+  width: 35px;
+  height: 35px;
+  margin-left: -42px;
+  margin-top: 7px;
+}
+
+.profile-button {
+  position: absolute;
+  width: 35px;
+  height: 35px;
+  margin-top: 7px;
+  margin-left: -152px;
+}
+
+.menu-cover:hover {
+  width: 100px;
+  transition: ease-in-out 0.4s;
+  cursor: pointer;
 }
 </style>
